cfb3de26c569ee4f59849b4e18dd17236b259b9d
rudak <roiduboul@gmail.com>
2014-08-23 03:57:09 +0200

    maj

---
+++ RudakRssBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rudak\Bundle\RssBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RudakRssBundle extends Bundle
+{
+
+}
