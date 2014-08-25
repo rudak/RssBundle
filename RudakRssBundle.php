87725726027a8bd637a46a86cdbc5578a83a3fcd
rudak <roiduboul@gmail.com>
2014-08-25 16:12:28 +0200

    modif target

--- RudakRssBundle.php
+++ RudakRssBundle.php
@@ -3,9 +3,10 @@
 namespace Rudak\Bundle\RssBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class RudakRssBundle extends Bundle
 {
-
+    public function __construct()
+    {
+    }
 }
