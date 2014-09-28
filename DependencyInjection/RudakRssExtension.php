3bee2033971fc1ad94b88b3d51247e7586499140
rudak <roiduboul@gmail.com>
2014-09-28 22:22:41 +0200

    change parameters injection system

--- DependencyInjection/RudakRssExtension.php
+++ DependencyInjection/RudakRssExtension.php
@@ -20,9 +20,11 @@ class RudakRssExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config        = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $container->setParameter('rssconfig', $config['rssparameters']);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
 }
